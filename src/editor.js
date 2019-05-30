b5ed82adbdcbfdc5f3e59cd2b34a50c5eb677ebd
Patrick Lafferty <patrick_lafferty@outlook.com>
2019-05-29 22:42:31 -0400

    Wire up layout input to allow display to change

--- src/editor.js
+++ src/editor.js
@@ -2,18 +2,35 @@ import React from 'react';
 import TabControl from './tab_control';
 import './css/editor.css';
 
-const Layout = () => <p>Layout</p>;
+class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.props.onLayoutChanged(e.target.value);
+    }
+
+    render() {
+        return (<input onChange={this.handleChange}/>);
+    }
+}
+
 const Code = () => <p>Code</p>;
 
-function Editor() {
-    return (
-        <section className="editor">
-            <TabControl>
-                <Layout label="layout"/>
-                <Code label="code"/>
-            </TabControl>
-        </section>
-    );
+class Editor extends React.Component {
+
+    render() {
+        return (
+            <section className="editor">
+                <TabControl>
+                    <Layout label="layout" onLayoutChanged={this.props.handleLayoutChanged}/>
+                    <Code label="code"/>
+                </TabControl>
+            </section>
+        );
+    }
 }
 
 export default Editor;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,26 @@ import Editor from './editor';
 import Display from './display';
 import './css/apollo.css';
 
-function ApolloApp() {
-    return (
-        <main className="app">
-            <DocumentationViewer />
-            <Editor />
-            <Display />
-        </main>
-    );
+class ApolloApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLayoutChanged = this.handleLayoutChanged.bind(this);
+        this.state = {layout: ''};
+    }
+
+    handleLayoutChanged(layout) {
+        this.setState({layout: layout});
+    }
+
+    render() {
+        return (
+            <main className="app">
+                <DocumentationViewer />
+                <Editor handleLayoutChanged={this.handleLayoutChanged}/>
+                <Display layout={this.state.layout} />
+            </main>
+        );
+    }
 }
 
 ReactDOM.render(
