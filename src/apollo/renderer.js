e5227573bb72834f1e3f81835c4346d2ebcc1520
Patrick Lafferty <patrick_lafferty@outlook.com>
2019-06-02 20:16:12 -0400

    Start porting the renderer from Saturn
    Only supports drawRectangle so far.

---
+++ src/apollo/renderer.js
@@ -0,0 +1,49 @@
+export class Renderer  {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext('2d');
+    }
+
+    drawRectangle(colour, bounds, clip) {
+        let windowWidth = this.canvas.clientWidth;
+        let windowHeight = this.canvas.clientHeight;
+
+        if (bounds.x >= windowWidth || bounds.y >= windowHeight) {
+            return;
+        }
+
+        let x = bounds.x;
+        let y = bounds.y;
+        let clippedWidth = bounds.width;
+        let clippedHeight = bounds.height;
+
+        if (bounds.x < clip.x) {
+            x = clip.x;
+            clippedWidth -= (clip.x - bounds.x);
+        }
+
+        if (bounds.y < clip.y) {
+            y = clip.y;
+            clippedHeight -= (clip.y - bounds.y);
+        }
+
+        if ((x + clippedWidth) >= (clip.x + clip.width)) {
+            clippedWidth = clip.x + clip.width - x;
+        }
+
+        if ((y + clippedHeight) >= (clip.y + clip.height)) {
+            clippedHeight = clip.y + clip.height - y;
+        }
+
+        if (clippedWidth < 0 || clippedHeight < 0) {
+            return;
+        }
+
+        this.context.fillStyle = colour;
+        this.context.fillRect(x, y, clippedWidth, clippedHeight);
+    }
+
+    drawText(layout, bounds, clip, backgroundColour) {
+
+    }
+}
